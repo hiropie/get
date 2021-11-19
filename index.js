e81803328c3ccf37191ded46190f16fb8c41c348
hiropie <hi610robo.outlook.jp>
2021-11-19 12:17:53 +0900

    ID

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
 const five = require("johnny-five")
 const client = require('socket.io-client')
+const readline = require('readline')
 
 const board = new five.Board({port: "COM6"}); //ポート名指定はWindowsで必要なため、
 // const accessPoint = "http://localhost:3000"  //localtest
 const accessPoint = "https://embryomonitor.herokuapp.com/" //heroku
+
+const main = async () => {
+      for(;;){
+          const chamberNo = await prompt('Enter Chamber No:(1 or 2)');
+          if(chamberNo == 1 || chamberNo == 2){
+              break
+          }
+      }
+};
+  
+/**
+ * ユーザーに値を入力させる
+ */
+const prompt = async (msg) => {
+    console.log(msg);
+    const answer = await question('> ');
+    return answer.trim();
+};
+
+/**
+ * 標準入力を取得する
+ */
+const question = (question) => {
+    const readlineInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+    });
+    return new Promise((resolve) => {
+        readlineInterface.question(question, (answer) => {
+        resolve(answer);
+        readlineInterface.close();
+        });
+    });
+};
+
+// 起動
+(async () => {
+    await main()
+})()
 
 board.on('ready', function () {
     let startTime = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));;
@@ -34,11 +74,14 @@ board.on('ready', function () {
         socket.emit("delete", null)
 
         setTimeout(function array(){
+            tmp2 = tmp.toFixed(2)
+            hum2 = hum.toFixed(2)
             let nowTime = Math.floor((new Date() - startTime)/1000)
             let data = {
+                connectId: chamberNo,
                 time: nowTime,
-                tmp: tmp.toFixed(2),
-                hum: hum.toFixed(2),
+                tmp: tmp2,
+                hum: hum2,
             }
             // console.log("sent")
             socket.emit("environment", data)
