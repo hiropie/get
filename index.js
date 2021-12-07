9d25fac22c1c4d113f35c383e7a55bacad4ed157
hiropie <hi610robo.outlook.jp>
2021-12-07 16:18:08 +0900

    check

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const client = require('socket.io-client');
 const readline = require('readline');
 
 
-// const accessPoint = "http://localhost:3000"  //localtest
+// const accessPoint = "http://localhost:8080"  //localtest
 const accessPoint = "https://embryomonitor.herokuapp.com/"; //heroku url
 let chamberNo = "";
 let COMNo = "";
+let timelist = [];
+let tmplist = [];
+let humlist = [];
+let data = {
+    connectId: chamberNo,
+    time: timelist,
+    tmp: tmplist,
+    hum: humlist
+};
 
 let startTime;
 let doFlag = 0;
@@ -79,8 +88,8 @@ function boardDo() {
         });
         bme280.on("data", function (e, data) {
             if (this.barometer.pressure > 90) {      //気圧の初回値が変なので、異常値は読み飛ばす
-                hum = this.hygrometer.relativeHumidity;
                 tmp = this.temperature.celsius;
+                hum = this.hygrometer.relativeHumidity;
                 //console.log("  celsius(摂氏)      : ", this.temperature.celsius);
                 //console.log("  fahrenheit(華氏)   : ", this.temperature.fahrenheit);
                 //console.log("  pressure(hPa)     : ", this.barometer.pressure *10);
@@ -91,70 +100,101 @@ function boardDo() {
         });
 
         const socket = client.connect(accessPoint);
-        socket.on('connect', () => {
-            socket.emit("delete", chamberNo)
+        socket.emit("delete", chamberNo);
+        console.log("delete sent");
 
+        socket.on("DataSentStart", () => {
+            console.log("Start")
+            let count = 0;
             setTimeout(function array() {
+                let nowTime = Math.floor((new Date() - startTime) / 1000);
                 tmp2 = tmp.toFixed(2);
                 hum2 = hum.toFixed(2);
 
-                pushcheck = humControl(hum2);
+                // pushcheck = humControl(hum2);
+                console.log("Count=> " + count);
+                console.log("time => " + nowTime);
+                console.log("tmp  => " + tmp2);
+                console.log("hum  => " + hum2);
+                console.log("-------------------------")
 
-                let nowTime = Math.floor((new Date() - startTime) / 1000);
-                let data = {
+
+                let data1 = {
                     connectId: chamberNo,
                     time: nowTime,
                     tmp: tmp2,
                     hum: hum2,
-                };
-                // console.log("sent")
-                socket.emit("environment", data);
-                setTimeout(array, 1000);
-            }, 1000);
+                }
+                // timelist[count] = nowTime;
+                // tmplist[count] = tmp2;
+                // humlist[count] = hum2;
+
+                // count++;
+                // if (count == 59) {
+                //     console.log("<<<<<<<<sent>>>>>>>>");
+                //     socket.emit("environment", data);
+                //     count = 0;
+                //     timelist = [];
+                //     tmplist = [];
+                //     humlist = [];
+                // }
+
+                socket.emit("environment", data1);
+                setTimeout(array, 60000);
+            }, 60000);
         });
 
-        function humControl(humidity) {
-            let nowTime = new Date() - startTime;
-            let check = 0;
+        socket.on("dataReq", () => {
+            console.log("<<<<<<<<sent>>>>>>>>");
+            socket.emit("environment", data);
+            count = 0;
+            timelist = [];
+            tmplist = [];
+            humlist = [];
+        })
 
-            if (humidity > target) {
-                doFlag = 1;
-            }
+        // function humControl(humidity) {
+        //     let nowTime = new Date() - startTime;
+        //     let check = 0;
 
-            if (doFlag == 0) {
-                if ((nowTime) > (injectionInterval * 60 * 1000 * count)) {
-                    console.log(nowTime);
-                    injection();
-                    count++;
-                    check = 1;
-                }
-            } else if (doFlag == 1 && humidity < target) {
-                downer++;
-            }
+        //     if (humidity > target) {
+        //         doFlag = 1;
+        //     }
 
-            if (downer > 9) {
-                console.log(nowTime);
-                injection();
+        //     if (doFlag == 0) {
+        //         if ((nowTime) > (injectionInterval * 60 * 1000 * count)) {
+        //             console.log(nowTime);
+        //             injection();
+        //             count++;
+        //             check = 1;
+        //         }
+        //     } else if (doFlag == 1 && humidity < target) {
+        //         downer++;
+        //     }
 
-                downer = 0;
-                check = 1;
-            }
-            return check;
-        }
+        //     if (downer > 9) {
+        //         console.log(nowTime);
+        //         injection();
 
-        function injection() {
-            ben.on();
-            push.on();
-            console.log("open");
-            setTimeout(() => {
-                push.off();
-            }, pushTime);
+        //         downer = 0;
+        //         check = 1;
+        //     }
+        //     return check;
+        // }
 
-            setTimeout(() => {
-                ben.off();
-                console.log("close");
-            }, openTime);
-        }
+        // function injection() {
+        //     ben.on();
+        //     push.on();
+        //     console.log("open");
+        //     setTimeout(() => {
+        //         push.off();
+        //     }, pushTime);
+
+        //     setTimeout(() => {
+        //         ben.off();
+        //         console.log("close");
+        //     }, openTime);
+        // }
     })
 }
 
